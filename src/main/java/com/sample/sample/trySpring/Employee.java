ad188385dc9f7226f85d584af830fc019c436e87
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2020-02-08 13:15:39 +0900

    complete basic practice

---
+++ src/main/java/com/sample/sample/trySpring/Employee.java
@@ -0,0 +1,11 @@
+package com.sample.sample.trySpring;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private int employeeId;
+    private String employeeName;
+    private int age;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO employee(employee_id, employee_name, age) VALUES ( 1,'YAMADA', 30);
